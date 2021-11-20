ccc603c0da45bf5a4c0f571b1b84807c56cad9c1
Ivan Lee <ivnyle@gmail.com>
2021-11-20 15:51:56 -0800

    save

---
+++ gradient_descent.py
@@ -0,0 +1,70 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gradient_descent(f, df, x0, step_size, tolerance):
+    """
+    Args:
+        f: function to minimize
+        df: derivative of f
+        x0: initial x value
+        step_size: step size
+        tolerance: tolerance for stopping condition
+    
+    Returns:
+        x_hist: history of x values as gradient descent updates x
+        f_hist: history of f(x) values as gradient descent updates x
+        iterations: number of iterations to reach tolerance
+    """
+    
+    x_hist = []
+    x_hist.append(x0)
+
+    f_hist = []
+    f_hist.append(f(x0))
+
+    iterations = 0
+
+    x = x0
+    
+    while True:
+        x -= step_size * df(x)
+
+        x_hist.append(x0)
+        f_hist.append(f(x0))
+        iterations += 1
+
+        if abs(f_hist[-1] - f_hist[-2]) < tolerance:
+            break
+
+    return x_hist, f_hist, iterations
+    
+
+if __name__ == '__main__':
+    
+    def f(x: np.ndarray) -> float:
+        """
+        f(x) = x_1**2 + x_2**2 + x_3**2 ... + x_n**2
+        """
+        return np.linalg.norm(x)**2
+
+    def df(x: np.ndarray) -> np.ndarray:
+        """
+        df(x) = 2 * x
+        """
+        return 2 * x
+
+    x0 = np.array([2., 4., -1., 1.])
+    step_size = 0.001
+    tolerance = 1e-10
+
+    x_hist, f_hist, iterations = gradient_descent(f, df, x0, step_size, tolerance)
+    print(f'final x value: {x_hist[-1]}')
+    print(f'final f value: {f_hist[-1]}')
+    print(f'iterations: {iterations}')
+    
+    # plot f_hist    
+    plt.plot(f_hist)
+    plt.xlabel('iteration')
+    plt.ylabel('$f(x)$')
+    plt.show()
