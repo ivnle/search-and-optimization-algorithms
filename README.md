d1de93756138a843136b1f0d2d7dcb7c2977c6c8
Ivan Lee <ivnyle@gmail.com>
2021-12-05 14:32:54 -0800

    save

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# search-and-optimization-algorithms
+# Search and Optimization Algorithms
 Implementations and visualizations of core search and optimization algorithms.
+
+## Gradient Descent 
+![](figures/grad_desc.png)
+
+## Newton Descent
+
+## Conjugate Descent
+
+## Simulated Annealing
+
+## Cross-Entropy Methods
+
+## Search Gradient
+
+## A* search
+
+## minimax search, 
+
+## (extra) RRT (rapidly-exploring random trees)
+TODO
+
+## Value iteration, 
+
+## Policy iteration
+
+## Monte Carlo Policy Evaluation 
+
+## Temporal Difference Policy Evaluation
+
+## Tabular Q learning
+
+![](figures/tab_q_learning.png)
+
+## Deep Q learning
+TODO
+
+## (extra) PPO (Proximal Policy Optimization)
+TODO
+
+## Monte Carlo Tree Search
+TODO
--- deep_q_learning.py
+++ deep_q_learning.py
@@ -7,106 +7,64 @@ from policy_iteration import policy_iteration, heatmap_value_func
 import random
 from tqdm import tqdm
 from collections import deque
+import matplotlib.pyplot as plt
+import math
 
+class LinearNN(nn.Module):
+    """
+    3 layer densely connected network.
+    """
 
-class DQN(nn.Module):
-
-    def __init__(self, state_space_dim, action_space_dim):
+    def __init__(self, state_dim, action_dim, hidden_dim=64):
         super().__init__()
 
         self.linear = nn.Sequential(
-            nn.Linear(state_space_dim, 64),
+            nn.Linear(state_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(64, 64*2),
+            nn.Linear(hidden_dim, hidden_dim*2),
             nn.ReLU(),
-            nn.Linear(64*2, action_space_dim)
+            nn.Linear(hidden_dim*2, action_dim)
         )
 
     def forward(self, x):
-        x = x.to(device)
         return self.linear(x)
 
 
 class ReplayMemory(object):
 
     def __init__(self, capacity):
-        # Define a queue with maxlen "capacity"
         self.memory = deque(maxlen=capacity)
 
     def push(self, state, action, next_state, reward):
-        # Add the tuple (state, action, next_state, reward) to the queue
         self.memory.append((state, action, next_state, reward))
 
     def sample(self, batch_size):
         # Get all the samples if the requested batch_size is higher than the number of sample currently in the memory
-        batch_size = min(batch_size, len(self))
+        # batch_size = min(batch_size, len(self))
         # Randomly select "batch_size" samples and return the selection
         return random.sample(self.memory, batch_size)
 
     def __len__(self):
-        # Return the number of samples currently stored in the memory
         return len(self.memory)
 
 
-def choose_action_epsilon_greedy(net, state, epsilon):
-
-    if epsilon > 1 or epsilon < 0:
-        raise Exception('The epsilon value must be between 0 and 1')
-
-    # Evaluate the network output from the current state
+def choose_action(net, state, epsilon, device):
+    
     with torch.no_grad():
         net.eval()
-        # Convert the state to tensor
-        state = torch.tensor(state, dtype=torch.float32)
-        net_out = net(state)
+        state = torch.tensor(state, dtype=torch.float32, device=device)
+        q_values = net(state)
 
-    # Get the best action (argmax of the network output)
-    best_action = int(net_out.argmax())
-    # Get the number of possible actions
-    action_space_dim = net_out.shape[-1]
+    best_action = int(q_values.argmax())
+    action_dim = q_values.shape[-1]
 
     # Select a non optimal action with probability epsilon, otherwise choose the best action
     if random.random() < epsilon:
-        # List of non-optimal actions (this list includes all the actions but the optimal one)
-        non_optimal_actions = [a for a in range(
-            action_space_dim) if a != best_action]
-        # Select randomly from non_optimal_actions
-        action = random.choice(non_optimal_actions)
+        action = random.randrange(action_dim)
     else:
-        # Select best action
         action = best_action
 
-    return action, net_out.cpu().numpy()
-
-
-def choose_action_softmax(net, state, temperature):
-
-    if temperature < 0:
-        raise Exception(
-            'The temperature value must be greater than or equal to 0 ')
-
-    # If the temperature is 0, just select the best action using the eps-greedy policy with epsilon = 0
-    if temperature == 0:
-        return choose_action_epsilon_greedy(net, state, 0)
-
-    # Evaluate the network output from the current state
-    with torch.no_grad():
-        net.eval()
-        state = torch.tensor(state, dtype=torch.float32)
-        net_out = net(state)
-
-    # Apply softmax with temp
-    # set a minimum to the temperature for numerical stability
-    temperature = max(temperature, 1e-8)
-    softmax_out = nn.functional.softmax(
-        net_out/temperature, dim=0).cpu().numpy()
-
-    # Sample the action using softmax output as mass pdf
-    all_possible_actions = np.arange(0, softmax_out.shape[-1])
-    # this samples a random element from "all_possible_actions" with the probability distribution p (softmax_out in this case)
-    action = np.random.choice(all_possible_actions, p=softmax_out)
-
-    return action, net_out.cpu().numpy()
+    return action, q_values.cpu().numpy()
 
 
 def update_step(policy_net, target_net, replay_mem, gamma, optimizer, loss_fn, batch_size):
@@ -115,7 +73,7 @@ def update_step(policy_net, target_net, replay_mem, gamma, optimizer, loss_fn, b
     batch = replay_mem.sample(batch_size)
     batch_size = len(batch)
 
-    # Create tensors for each element of the batch
+    # batch: (state, action, next_state, reward)
     states = torch.tensor([s[0] for s in batch],
                           dtype=torch.float32, device=device)
     actions = torch.tensor([s[1] for s in batch],
@@ -159,104 +117,74 @@ def update_step(policy_net, target_net, replay_mem, gamma, optimizer, loss_fn, b
     nn.utils.clip_grad_norm_(policy_net.parameters(), 2)
     optimizer.step()
 
+def update_epsilon(epsilon, steps):
+    EPS_START = 0.9
+    EPS_END = 0.05
+    EPS_DECAY = 200
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps / EPS_DECAY)
+    return eps_threshold
+
 
 if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # environment = 'FrozenLake-v0'
-    environment = 'Acrobot-v1'
-    # environment = "FrozenLake8x8-v0"
-    # environment = "Taxi-v2"
-    # environment = "CartPole-v1"
-    initial_value = 5
-    num_iterations = 800
-    # We compute the exponential decay in such a way the shape of the exploration profile does not depend on the number of iterations
-    exp_decay = np.exp(-np.log(initial_value) / num_iterations * 6)
-    exploration_profile = [initial_value *
-                           (exp_decay ** i) for i in range(num_iterations)]
+    # environment = 'Acrobot-v1'
+    #environment = "FrozenLake8x8-v1"
+    #environment = "Taxi-v3"
+    environment = "CartPole-v1"
 
-    # Create environment
-    env = gym.make('Acrobot-v1')  # Initialize the Gym environment
-    env.seed(0)  # Set a random seed for the environment (reproducible results)
-
-    # Get the shapes of the state space (observation_space) and action space (action_space)
+    env = gym.make(environment)
     state_space_dim = env.observation_space.shape[0]
     action_space_dim = env.action_space.n
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    print(f"STATE SPACE SIZE: {state_space_dim}")
-    print(f"ACTION SPACE SIZE: {action_space_dim}")
 
     # Set random seeds
+    env.seed(0)
     torch.manual_seed(0)
     np.random.seed(0)
 
-    # PARAMETERS
-    gamma = 0.99   # gamma parameter for the long term reward
-    replay_memory_capacity = 10000   # Replay memory capacity
-    # lr = 1e-2   # Optimizer learning rate
-    #lr = 1e-4
+    # Set parameters
+    gamma = 0.99
+    epsilon = 0.1
+    replay_memory_capacity = 10000
     lr = 1e-3
-    # Number of episodes to wait before updating the target network
     target_net_update_steps = 10
-    batch_size = 256   # Number of samples to take from the replay memory for each update
-    # Penalty to the reward when we are in a bad state (in this case when the pole falls down)
-    bad_state_penalty = 0
-    # Minimum samples in the replay memory to enable the training
+    batch_size = 256
+    episodes = 200
     min_samples_for_training = 1000
 
-    # Initialize the replay memory
+    # Initialize replay memory
     replay_mem = ReplayMemory(replay_memory_capacity)
 
-    # Initialize the policy network
-    policy_net = DQN(state_space_dim, action_space_dim).to(device)
-
-    # Initialize the target network with the same weights of the policy network
-    target_net = DQN(state_space_dim, action_space_dim).to(device)
-    # This will copy the weights of the policy network to the target network
+    # Initialize policy and target networks. The target network shares the same initial weights as the policy network.
+    policy_net = LinearNN(state_space_dim, action_space_dim).to(device)
+    target_net = LinearNN(state_space_dim, action_space_dim).to(device)
     target_net.load_state_dict(policy_net.state_dict())
 
-    # Initialize the optimizer
-    # The optimizer will update ONLY the parameters of the policy network
+    # Only optimize the parameters of the policy network
     optimizer = torch.optim.Adam(policy_net.parameters(), lr=lr)
 
-    # Initialize the loss function (Huber loss)
+    # Use Huber loss: https://en.wikipedia.org/wiki/Huber_loss
     loss_fn = nn.SmoothL1Loss()
 
-    env = gym.make('Acrobot-v1')
-    env.seed(0)  # Set a random seed for the environment (reproducible results)
+    reward_hist = []
+    steps = 0
 
-    # # This is for creating the output video in Colab, not required outside Colab
-    # env = wrap_env(env, video_callable=lambda episode_id: episode_id % 100 == 0) # Save a video every 100 episodes
-
-    plotting_rewards = []
-
-    for episode_num, tau in enumerate(tqdm(exploration_profile)):
-
-        # Reset the environment and get the initial state
+    for episode_num, traj in enumerate(tqdm(range(episodes))):
         state = env.reset()
-        # Reset the score. The final score will be the total amount of steps before the pole falls
         score = 0
         done = False
 
-        # Go on until the pole falls off
         while not done:
+            action, q_values = choose_action(policy_net, state, epsilon, device)
 
-            # Choose the action following the policy
-            action, q_values = choose_action_softmax(
-                policy_net, state, temperature=tau)
-
-            # Apply the action and get the next state, the reward and a flag "done" that is True if the game is ended
+            steps += 1
+            epsilon = update_epsilon(epsilon, steps)
+            
             next_state, reward, done, info = env.step(action)
 
-            # Update the final score (+1 for each step)
-            score += reward
-
-            # Apply penalty for bad state
-            if done:  # if the pole has fallen down
-                reward += bad_state_penalty
-                next_state = None
-
-            # Update the replay memory
             replay_mem.push(state, action, next_state, reward)
+            score += reward
 
             # Update the network
             # we enable the training only if we have enough samples in the replay memory, otherwise the training will use the same samples too often
@@ -264,10 +192,6 @@ if __name__ == '__main__':
                 update_step(policy_net, target_net, replay_mem,
                             gamma, optimizer, loss_fn, batch_size)
 
-            # Visually render the environment (disable to speed up the training)
-            # env.render()
-
-            # Set the current state for the next iteration
             state = next_state
 
         # Update the target network every target_net_update_steps episodes
@@ -276,10 +200,17 @@ if __name__ == '__main__':
             # This will copy the weights of the policy network to the target network
             target_net.load_state_dict(policy_net.state_dict())
 
-        plotting_rewards.append(score)
-        # Print the final score
-        # Print the final score
+        reward_hist.append(score)
+
         print(
-            f"EPISODE: {episode_num + 1} - FINAL SCORE: {score} - Temperature: {tau}")
+            f"EPISODE: {episode_num + 1} - FINAL SCORE: {score} - Temperature: {traj}")
 
     env.close()
+
+    plt.plot(reward_hist)
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.legend()
+    
+    #save plot
+    plt.savefig('figures/deep_q_learning.png')
